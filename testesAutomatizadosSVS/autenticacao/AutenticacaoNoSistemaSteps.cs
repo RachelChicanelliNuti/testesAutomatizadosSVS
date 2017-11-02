37bbb96f372d6a96ebffb794a618badca2eee236
RACHEL-NOTE\rache <rachel.chicanelli@nuti.ic.ufmt.br>
2017-11-02 20:02:15 -0300

    Initial commit

---
+++ testesAutomatizadosSVS/autenticacao/AutenticacaoNoSistemaSteps.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace testesAutomatizadosSVS.feature.steps
+{
+    [Binding]
+    public class AutenticacaoNoSistemaSteps
+    {
+        private IWebDriver driver = new ChromeDriver();
+
+        [Given(@"que estou na página de autenticacao")]
+        public void DadoQueEstouNaPaginaDeAutenticacao()
+        {
+            driver.Navigate().GoToUrl("http://192.168.207.157/");
+        }
+        
+        [When(@"eu preencho o campo CPF com o valor '(.*)'")]
+        public void QuandoEuPreenchoOCampoCPFComOValor(string cpf)
+        {
+            IWebElement cpfTxt = driver.FindElement(By.Id("CPF"));
+            cpfTxt.SendKeys(cpf);
+        }
+        
+        [When(@"preencha o campo Senha com o valor '(.*)'")]
+        public void QuandoPreenchaOCampoSenhaComOValor(string senha)
+        {
+            IWebElement senhaTxt = driver.FindElement(By.Id("Senha"));
+            senhaTxt.SendKeys(senha);
+        }
+        
+        [When(@"clico no botão Entrar")]
+        public void QuandoClicoNoBotaoEntrar()
+        {
+            driver.FindElement(By.XPath("//*[@value='Entrar']")).Click();
+
+
+        }
+        
+        [Then(@"vejo na tela '(.*)'")]
+        public void EntaoVejoNaTela(string mensagem)
+        {
+            bool achouTexto = driver.PageSource.Contains(mensagem);
+            if (!achouTexto)
+            {
+                throw new Exception("Erro encontrado: \n" +
+                    "CLASSE: AutenticacaoNoSistemaSteps\n" +
+                    "MÉTODO: EntaoVejoNaTela.\n" +
+                    "DESCRIÇÃO: Mensagem não exibida na tela: '" + mensagem+"'");
+            }
+        }
+        [AfterScenario]
+        public void FechaBrowser()
+        {
+            driver.Dispose();
+        }
+    }
+}
---
+++ testesAutomatizadosSVS/autenticacao/autenticarSeNoSistema.feature
@@ -0,0 +1,19 @@
+﻿Funcionalidade: Autenticação no sistema
+Para acessar tela de escolha de módulo
+Enquanto usuário
+Eu gostaria de me autenticar no sistema e escolher o perfil
+
+Esquema do Cenário: Autenticação com sucesso
+	Dado que estou na página de autenticacao 
+	Quando eu preencho o campo CPF com o valor <cpf>
+	E preencha o campo Senha com o valor <senha>
+	Quando clico no botão Entrar
+	Então vejo na tela <mensagem>
+
+	Exemplos:
+	| cpf           | senha        | mensagem             |
+	| '11111111111' | '1111111111' | 'selecione o perfil' |
+	| '22222222222' | '2222222222' | ''                   |
+	| '99999999999' | ''           | 'Senha obrigatório'  |
+	| ''            | '1111111111' | 'CPF obrigatório'    |
+	
